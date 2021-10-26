38d95b6304ae2f619ca4910079f6e971633ed56d
lalugue <51893731+lalugue@users.noreply.github.com>
2021-10-27 07:51:33 +0800

    add components to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from "./components/Header";
+import Card from "./components/Card";
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      Hello World!
+      <Header/>
+      <Card/>
+      <Footer/>
     </div>
   );
 }
