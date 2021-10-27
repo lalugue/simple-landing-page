f8d87c39120dcdf84ed59277e6f96432985af88c
lalugue <51893731+lalugue@users.noreply.github.com>
2021-10-27 09:43:29 +0800

    update subtexts

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Card from "./components/Card";
 import Footer from './components/Footer';
 
 function App() {
+
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin massa risus, varius sed hendrerit nec, vestibulum eget odio. Vestibulum non sem gravida, malesuada dui eu, dapibus ligula. Duis efficitur lacinia nisl quis auctor. Nullam et nisi vitae tortor pellentesque faucibus. Aliquam pulvinar tristique tortor sit amet varius. Pellentesque porta tristique dolor, et malesuada arcu luctus vel. Duis sem nulla, tempor quis ornare non, dignissim in mi.";
+
   return (
     <div className="App">
       <Header text="Hello World!" subtext="lorem ipsum"/>
-      <Card text="Lorem" subtext="Dolor sit amet, consectetur adipiscing"/>
-      <Card text="Ipsum" subtext="Ipsum dolor sit amet, consectetur adipiscing elit"/>
-      <Card text="Dolor" subtext="Dolor sit amet, consectetur adipiscing elit sed"/>
+      <Card text="Lorem" subtext={text}/>
+      <Card text="Ipsum" subtext={text}/>
+      <Card text="Dolor" subtext={text}/>
       <Footer/>
     </div>
   );
