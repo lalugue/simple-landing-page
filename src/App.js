4562d10216fb2ad0ba040d2681b2a3a708ac4567
lalugue <51893731+lalugue@users.noreply.github.com>
2022-05-28 00:58:15 +0800

    make remaining classes lower-case

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin massa risus, varius sed hendrerit nec, vestibulum eget odio. Vestibulum non sem gravida, malesuada dui eu, dapibus ligula. Duis efficitur lacinia nisl quis auctor. Nullam et nisi vitae tortor pellentesque faucibus. Aliquam pulvinar tristique tortor sit amet varius. Pellentesque porta tristique dolor, et malesuada arcu luctus vel. Duis sem nulla, tempor quis ornare non, dignissim in mi.";
 
   return (
-    <div className="App">
+    <div className="app">
       <Header text="Hello World!" subtext="lorem ipsum" />
-      <Card text="Lorem" subtext={text} classProp="LightCard" />
-      <Card text="Ipsum" subtext={text} classProp="DarkCard" />
-      <Card text="Dolor" subtext={text} classProp="LightCard" />
+      <Card text="Lorem" subtext={text} classProp="light-card" />
+      <Card text="Ipsum" subtext={text} classProp="dark-card" />
+      <Card text="Dolor" subtext={text} classProp="light-card" />
       <Footer />
     </div>
   );
