c48bf8a169a089d60a1de467b77f13a45e159255
lalugue <51893731+lalugue@users.noreply.github.com>
2022-05-28 00:56:19 +0800

    make header class smaller-case

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import "./Header.css";
 
 function Header(props) {
   return (
-    <div className="Header">
+    <div className="header">
       <h1>{props.text}</h1>
       <p>{props.subtext}</p>
     </div>
